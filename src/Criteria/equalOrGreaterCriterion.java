a77d32329d568b440a16a981282a9d4378251888
Pete <koala2324@outlook.com>
2026-06-29 11:06:06 -0500

    test

---
+++ src/Criteria/equalOrGreaterCriterion.java
@@ -0,0 +1,14 @@
+package Criteria;
+
+import DataAccess.StudentDTO;
+
+public class equalOrGreaterCriterion implements Criterion {
+
+	
+	@Override
+	public boolean conditionIsSatisfied(StudentDTO myStudent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
---
+++ src/Criteria/equalOrLessCriterion.java
@@ -0,0 +1,15 @@
+package Criteria;
+
+import DataAccess.StudentDTO;
+
+public class equalOrLessCriterion implements Criterion {
+
+	
+	
+	@Override
+	public boolean conditionIsSatisfied(StudentDTO myStudent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
